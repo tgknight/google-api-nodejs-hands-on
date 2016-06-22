4d2e9dee0624b92652fea87be039dc9bbc0f9760
Thanapat Bhunnachet <tgcruasde@gmail.com>
2016-06-22 15:05:20 +0700

    Implement dataproc wrapper, test OAuth2

--- index.js
+++ index.js
@@ -3,11 +3,33 @@
 const Google = require('./lib')
 const google = new Google({
   projectId: 'siwat-project',
-  keyFilename: '/api_key/key.json',
+  clientConfig: {
+    CLIENT_ID: '631579447125-55r9la6smdab5bc19ekhkrmcjplom5ak.apps.googleusercontent.com',
+    CLIENT_SECRET: 'rTS_9_ZM2nGAQkZwWyA780Bq',
+    REDIRECT_URL: 'http://localhost/oauth2callback',
+  },
+  credentials: {
+    access_token: 'ya29.Ci8JAxnBtwCOTN1ZZhJ1h3KzGEYKlC6AD4qA-hxravCypGiES1UJA9d_36RggZCNsg',
+    refrest_token: '1/nEFTBbMFOUCdkz6T51GS9UILOgVs_5uMVKmM9GlDPyQ',
+  },
+
+  // keyFilename: '/api_key/siwat-project-4b39dd4cc219.json',
 })
-const bucket = google.storage({
+/* const bucket = google.storage({
   location: 'asia-east1',
-}).bucket('datana-dev')
+}).bucket('datana-dev') */
+
+const cluster = google.dataproc().cluster({
+  bucket: 'datana-dev',
+  clusterName: 'datana-cluster-apis',
+})
+
+cluster.create({
+  zone: 'asia-east1',
+  masterType: 'n1-standard-4',
+  workerType: 'n1-standard-4',
+  workerNodeNumber: 2,
+}).then(res => console.log(res)).catch(err => console.log(err))
 /* const file = bucket.file('userdata/test.txt')
 const file2 = bucket.file('userdata/README.md')
 
@@ -40,8 +62,8 @@ deleted.delete().then(res => console.log(res)).catch(err => console.log(err)) */
 
 // file.upload('.jscsrc').then(res => console.log(res)).catch(err => console.log(err))
 
-const folder = bucket.file('newfolderja/')
-folder.deleteFolder().then(res => console.log(res)).catch(err => console.log(err))
+// const folder = bucket.file('newfolderja/')
+// folder.deleteFolder().then(res => console.log(res)).catch(err => console.log(err))
 
 /* folder.createFolder().then(res => {
   console.log(res)
